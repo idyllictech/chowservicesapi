3dfb24f844ed072db379fd0d9b215015cb10e09d
Yinka <digits.solutions@gmail.com>
2024-02-09 09:44:56 +0100

    launch new start

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -10,7 +10,7 @@ class ServicesController extends Controller
     public function services()
     {
         // Add your logic to retrieve and return services
-        return response()->json(['services' => 'Your services data here'], 200);
+        return response()->json(['services' => 'Welcome to FoodHup API Your services data here'], 200);
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+
+    return redirect('/api/v1.0/chowhubs/services');
+
+    //return redirect()->away('/api/v1.0/chowhubs/services');
 });
